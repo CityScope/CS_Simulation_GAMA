ecd9d959ebf0df19838442348f214383e35e32b0
crisjf <cristianjf@dhcp-18-21-158-169.dyn.mit.edu>
2019-10-18 15:18:56 -0400

    documented combineSnaps script

--- CS_CityScope_GAMA/models/combineSnaps.py
+++ CS_CityScope_GAMA/models/combineSnaps.py
@@ -1,18 +1,33 @@
-import os 
-outFname = 'test.mp4'
-fnames = [f for f in os.listdir('.') if 'png'==f.split('.')[-1]]
-cycles = [int(f.split('cycle_')[-1].split('_')[0].replace('.png','')) for f in fnames]
-order = sorted(range(len(cycles)), key=lambda k: cycles[k])
+import os
+def combine(outFname = 'test.mp4',frameRate = '60',aspectRatio='2560x1049'):
+	'''
+	Combine set of png files into a mp4 video.
 
-digits = 3
-j = 0
-for i in order:
-	src = fnames[i]
-	dst = 'cycle_'+('0'*digits+str(j))[-3:]+'.png'
-	print(src,dst)
-	os.rename(src, dst)
-	j+=1
+	Parameters
+	----------
+	outFname : str ('test.mp4')
+		Filename for outfile
+	frameRate : int (60)
+		Frame rate of video.
+	aspectRatio : str ('2560x1049')
+		Aspect ratio of images.
+	'''
+	fnames = [f for f in os.listdir('.') if 'png'==f.split('.')[-1]]
+	cycles = [int(f.split('cycle_')[-1].split('_')[0].replace('.png','')) for f in fnames]
+	order = sorted(range(len(cycles)), key=lambda k: cycles[k])
 
-cmd = 'ffmpeg -r 60 -f image2 -s 2560x1049 -i cycle_%03d.png -vf scale=1280:-2 -vcodec libx264 -crf 25  -pix_fmt yuv420p '+outFname
-print(cmd)
-os.system(cmd)
+	digits = 4
+	j = 0
+	for i in order:
+		src = fnames[i]
+		dst = 'cycle_'+('0'*digits+str(j))[-3:]+'.png'
+		print(src,dst)
+		os.rename(src, dst)
+		j+=1
+
+	cmd = 'ffmpeg -r '+str(frameRate)+' -f image2 -s '+aspectRatio+' -i cycle_%0'+str(digits)+'d.png -vf scale=1280:-2 -vcodec libx264 -crf 25  -pix_fmt yuv420p '+outFname
+	print(cmd)
+	os.system(cmd)
+
+if __name__ == "__main__":
+	combine()
