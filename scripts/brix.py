6eb20115755519be195b3abf57c0043d042afa69
carloslopllo <carlos.lopez.llorente@alumnos.upm.es>
2024-10-30 14:01:14 +0100

    websocket -> websockets

--- scripts/brix.py
+++ scripts/brix.py
@@ -1,13 +1,10 @@
 import asyncio
 import json
 import os
-from threading import Thread
 from typing import Dict
 
-import rel
-import websocket
-from websockets.asyncio import server
-from websockets.exceptions import ConnectionClosed
+from websockets.asyncio import client,server
+from websockets.exceptions import ConnectionClosed,InvalidHandshake,InvalidURI
 
 from gama_client.message_types import MessageTypes
 from gama_client.sync_client import GamaSyncClient
@@ -18,17 +15,8 @@ class Brix():
     geogrid_data={}
     geogrid={}
 
-    def __init__(self,
-        table_name=None,
-        quietly=False,
-        host_mode='remote',
-        host_name=None,
-        core=False,
-        core_name=None,
-        core_description=None,
-        core_category=None,
-        save=False
-    ):
+    def __init__(self,table_name=None,quietly=False,host_mode='remote',host_name=None,
+        core=False,core_name=None,core_description=None,core_category=None,save=False):
 
         self.host='127.0.0.1:8080' if host_mode=='local' else (host_name.strip('/') if host_name else self.remote_host)
         self.secure_protocol='' if host_mode=='local' else 's'
@@ -46,78 +34,54 @@ class Brix():
         self.core_name=core_name
         self.core_description=core_description
         self.core_category=core_category
+        self.ws=None
 
-        self.ws_server_thread=Thread(target=self.start_websocket_server,daemon=True)
-        self.ws_gama_client=Thread(target=self.start_gama_client,daemon=True)
-
-        if not self.quietly:
-            websocket.enableTrace(True)
-
-        self.ws=websocket.WebSocketApp(self.cityIO_wss,
-            on_open=self.on_open,
-            on_message=self.on_message,
-            on_error=self.on_error,
-            on_close=self.on_close
-        )
-
-    def on_message(self,ws,message):
+    async def on_message(self,message):
         dict_rec=json.loads(message)
         message_type=dict_rec['type']
+        content=dict_rec.get('content',{})
+        table_name=content.get('tableName')
 
-        if(message_type=='TABLE_SNAPSHOT'):
-            table_name=dict_rec['content']['tableName']
-            self.geogrid_data[table_name]=dict_rec['content']['snapshot']['GEOGRIDDATA']
-            self.geogrid[table_name]=dict_rec['content']['snapshot']['GEOGRID']
-            self.ws_server_thread.start()
-            self.ws_gama_client.start()
+        if message_type=='TABLE_SNAPSHOT':
+            self.geogrid_data[table_name]=content['snapshot']['GEOGRIDDATA']
+            self.geogrid[table_name]=content['snapshot']['GEOGRID']
 
-        elif(message_type=='GEOGRIDDATA_UPDATE'):
-            table_name=dict_rec['content']['tableName']
-            self.geogrid_data[table_name]=dict_rec['content']['geogriddata']
+        elif message_type=='GEOGRIDDATA_UPDATE':
+            self.geogrid_data[table_name]=content['geogriddata']
 
-        elif(self.core and message_type=='SUBSCRIPTION_REQUEST'):
-            requester=dict_rec['content']['table']
-            self.send_message(json.dumps({"type":"SUBSCRIBE","content":{"gridId":requester}}))
+        elif self.core and message_type in {'SUBSCRIPTION_REQUEST','SUBSCRIPTION_REMOVAL_REQUEST'}:
+            action="SUBSCRIBE" if message_type=='SUBSCRIPTION_REQUEST' else "UNSUBSCRIBE"
+            await self.send_message({"type":action,"content":{"gridId":content['table']}})
 
-        elif(self.core and message_type=='SUBSCRIPTION_REMOVAL_REQUEST'):
-            requester=dict_rec['content']['table']
-            self.send_message(json.dumps({"type":"UNSUBSCRIBE","content":{"gridId":requester}}))
-
-    def on_error(self,ws,error):
-        print(error)
-
-    def on_close(self,ws,close_status_code,close_msg):
-        print("## Connection closed")
-
-    def on_open(self,ws):
+    async def on_open(self):
         print("## Opened connection")
-        if self.core:
-            self.send_message(json.dumps({"type":"CORE_MODULE_REGISTRATION","content":{"name":self.core_name,"description":self.core_description,"moduleType":self.core_category}}))
-        else:
-            self.send_message(json.dumps({"type":"SUBSCRIBE","content":{"gridId":self.table_name}}))
+        message_type="CORE_MODULE_REGISTRATION" if self.core else "SUBSCRIBE"
+        content=(
+            {"name":self.core_name,"description":self.core_description,"moduleType":self.core_category}
+            if self.core else {"gridId":self.table_name}
+        )
+        await self.send_message({"type":message_type,"content":content})
 
-    def send_message(self,message):
-        self.ws.send(message)
+    async def send_message(self,message:Dict):
+        await self.ws.send(json.dumps(message))
 
-    def _send_indicators(self,layers,numeric):
-        if(layers is not None and numeric is not None):
-            message={"type":"MODULE","content":{"gridId":self.table_name,"save":self.save,"moduleData":{"layers":layers,"numeric":numeric}}}
+    async def _send_indicators(self,layers,numeric):
+        message={"type":"MODULE","content":{"gridId":self.table_name,"save":self.save,"moduleData":{}}}
 
-        elif(layers is not None):
-            message={"type":"MODULE","content":{"gridId":self.table_name,"save":self.save,"moduleData":{"layers":layers}}}
+        if layers is not None:
+            message["content"]["moduleData"]["layers"]=layers
 
-        elif(numeric is not None):
-            message={"type":"MODULE","content":{"gridId":self.table_name,"save":self.save,"moduleData":{"numeric":numeric}}}
+        if numeric is not None:
+            message["content"]["moduleData"]["numeric"]=numeric
 
-        self.send_message(json.dumps(message))
+        await self.send_message(message)
 
-    def listen(self):
-        self.ws.run_forever(dispatcher=rel,reconnect=5)
-        rel.signal(2,rel.abort)  # Keyboard Interrupt
-        rel.dispatch()
-
-    def stop(self):
-        self.ws.close()
+    async def listen(self):
+        try:
+            self.ws=await client.connect(uri=self.cityIO_wss)
+            await self.ws.wait_closed()
+        except (InvalidURI,OSError,InvalidHandshake,TimeoutError) as e:
+            print(f"Error:{e}")
 
     async def handle_connection(self,websocket):
         try:
@@ -127,30 +91,26 @@ class Brix():
                 r=json.loads(message)
 
                 for item in r:
-                    if item["type"]=="bar":
-                        for mean in item["data"]:
-                            numeric.append({"viz_type":"bar","name":mean,"value":item["data"][mean]["value"],"description":item["data"][mean]["description"]})
+                    item_type=item["type"]
+                    item_data=item["data"]
 
-                    elif item["type"]=="radar":
-                        for mean in item["data"]:
-                            numeric.append({"viz_type":"radar","name":mean,"value":item["data"][mean]["value"],"description":item["data"][mean]["description"]})
-
+                    if item_type in {"bar","radar"}:
+                        for mean in item_data:
+                            numeric.append({"viz_type":item_type,"name":mean,"value":item_data[mean]["value"],
+                                            "description":item_data[mean]["description"]})
                     else:
                         layers.append(item)
 
-                self._send_indicators(layers,numeric)
+                await self._send_indicators(layers,numeric)
 
         except ConnectionClosed:
             print("Client disconnected")
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error:{e}")
 
     async def start_server(self):
-        ws=await server.serve(self.handle_connection,"localhost",8080,max_size=None)
+        ws=await server.serve(handler=self.handle_connection,host="localhost",port=8080,max_size=None)
         await ws.wait_closed()
-
-    def start_websocket_server(self):
-        asyncio.run(self.start_server())
 
     async def async_command_answer_handler(self,message:Dict):
         print("Here is the answer to an async command:\t",message)
@@ -159,7 +119,7 @@ class Brix():
         print("Here is the message from Gama-server:\t",message)
 
     async def gama_client(self):
-        client=GamaSyncClient("localhost",8000,self.async_command_answer_handler,self.gama_server_message_handler)
+        client=GamaSyncClient(url="localhost",port=8000,async_command_handler=self.async_command_answer_handler,other_message_handler=self.gama_server_message_handler)
         await client.connect(False)
 
         project_root=os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
@@ -167,17 +127,12 @@ class Brix():
 
         command_answer=client.sync_load(gaml_path,"gameit")
 
-        if "type" in command_answer.keys() and command_answer["type"]==MessageTypes.CommandExecutedSuccessfully.value:
+        if command_answer.get("type")==MessageTypes.CommandExecutedSuccessfully.value:
             await client.play(exp_id=command_answer["content"])
 
         while True:
             await asyncio.sleep(1)
 
-    def start_gama_client(self):
-        asyncio.run(self.gama_client())
-
 if __name__=="__main__":
     connection=Brix(table_name='volpe-habm',quietly=True)
-
-    connection.listen()
-    connection.stop()
+    asyncio.run(asyncio.gather(connection.gama_client(),connection.start_server(),connection.listen()))
